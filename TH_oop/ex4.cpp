f2ef0b1b395af16037074d9d781774b00438c476
V <lytranvinh.work@gmail.com>
2023-09-06 17:55:34 +0700

    loadding ex4 ...

--- TH_oop/ex4.cpp
+++ TH_oop/ex4.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 class Date{
     int day, month, year;
+    int so_ngay_trong_thang(int m, int y) {
+        if (m == 2) { // Tháng 2 cuả năm nhuận có 29 ngày còn các năm khác có 28 ngày
+            return check_nam_nhuan(y) ? 29 : 28;
+        } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+            return 30;
+        } else {
+            return 31;
+        }
+    }
 public:
     Date(){
         day = 1;
@@ -19,17 +28,18 @@ public:
             Date();
         }
     }
+    bool check_nam_nhuan(int y){
+        return ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0));
+    }
     bool check(int d, int m, int y){
-//
+        return d > 0 && m > 0 && m <= 12 && d < so_ngay_trong_thang(m, y);
     }
-    void show(){
+    void updateDate(int d, int m, int y){
+        if (check(d, m, y)){ // Ngày, tháng, năm nhập vào phải có trong thời gian thực
 //
-    }
-    void check_nam_nhuan(){
-//
-    }
-    void updateDate(){
-//
+        } else {
+            cout << "Error !!!" << endl;
+        }
     }
 
     int so_ngay_cach_nhau(Date A){
@@ -37,6 +47,11 @@ public:
     }
     int so_ngay_cach_nhau(int d, int m, int y){
 //
+    }
+    void show(){
+        cout << "Day: " << setw (2) << setfill('0') << day << ", month: " 
+                << setw (2) << setfill('0') << month << ", year: "
+                << year << endl;
     }
 };
 
