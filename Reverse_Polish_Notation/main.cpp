f458d6125818b513b7ac159495ec89d49beecd25
V <lytranvinh.work@gmail.com>
2023-10-04 19:11:46 +0700

    cmp

--- Reverse_Polish_Notation/main.cpp
+++ Reverse_Polish_Notation/main.cpp
@@ -1,81 +1,58 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-class RPN{
-    string input;
-    char top_st;
-public:
-    RPN(string input){
-        this->input = input;
-    }
-    bool check_toan_hang(char ch){
-        return (ch == '^') || (ch == '*') || (ch == '/') || 
-                (ch == '+') || (ch == '-');
-    }
-    int priorityCh(char ch){
-        if (ch == '^'){
-            return 3;
-        } else if (ch == '*' || ch == '/'){
-            return 2;
-        } else if (ch == '+' || ch == '-'){
-            return 1;
-        }
-        return 0;
-    }
-    string solve(){
-        stack<char> st;
-        string rpn = "";
-        int n = input.length();
-        for (int i = 0; i < n; i++){
-            if ((input[i] - '0') >= 0 && 
-                (input[i] - '0') <= 9){ 
-                    rpn += input[i];
-            } else if (input[i] == '('){
-                st.push('(');
-            } else if (input[i] == ')'){
-                top_st = st.top();
-                while (top_st != '(' && !st.empty()){
-                    rpn += top_st;
-                    st.pop();
-                    top_st = st.top();
-                }
-                st.pop();
-            } else if (check_toan_hang(input[i])){
-                if (st.empty()){
-                    st.push(input[i]);
-                } else{
-                    top_st = st.top();
-                    while (priorityCh(top_st) >= priorityCh(input[i]) &&
-                            !st.empty()){
-                        rpn += top_st;
-                        st.pop();
-                        top_st = st.top();
-                    }
-                    st.push(input[i]);
-                }
-            }
-        }
-        while(!st.empty()){
-            rpn += st.top();
-            st.pop();
-        }
-        return rpn;
-    }
-};
-
-void run(){
-    string str = "3+4*2/(1-5)^2";
-    cout << "Nhập vào biểu thức: ";
-    /* cin.ignore();
-    getline(cin, str); */
-    RPN rpn(str);
-    cout << "Biểu thức sau khi chuyển sang biểu thức nghịch Ba Lan: " << rpn.solve() << endl;
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 }
 
-int main(){
+int precedence(char op) {
+    if (op == '^') return 3;
+    if (op == '*' || op == '/') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
 
-    run();
+string infixToRPN(string infix) {
+    string rpn = "";
+    stack<char> opStack;
+
+    for (char c : infix) {
+        if (isalnum(c)) {
+            rpn += c;  // Nếu là toán hạng, thêm vào RPN
+        } else if (c == '(') {
+            opStack.push(c);
+        } else if (c == ')') {
+            while (!opStack.empty() && opStack.top() != '(') {
+                rpn += opStack.top();
+                opStack.pop();
+            }
+            opStack.pop();  // Loại bỏ '(' khỏi ngăn xếp
+        } else if (isOperator(c)) {
+            while (!opStack.empty() && precedence(c) <= precedence(opStack.top())) {
+                rpn += opStack.top();
+                opStack.pop();
+            }
+            opStack.push(c);
+        }
+    }
+
+    while (!opStack.empty()) {
+        rpn += opStack.top();
+        opStack.pop();
+    }
+
+    return rpn;
+}
+
+int main() {
+    string infixExpression = "11 + ( ( 10 - 2 ) * 6 ) + 7";
+    cout << "Nhap bieu thuc trung to: ";
+    cin >> infixExpression;
+
+    string rpnExpression = infixToRPN(infixExpression);
+    cout << "Bieu thuc nghich dao ba lan (RPN): " << rpnExpression << endl;
 
     return 0;
 }
